ea6b64b8581ab142ca3fbbe53fa9185ebf48ed21
Yulia <yuliaotrokova@gmail.com>
2022-12-02 10:25:14 +0300

    practice_1

---
+++ test/test_sample.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+def test_sample():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://www.saucedemo.com/")
+
+
+    assert driver.title == 'Swag Labs'
+
+    driver.quit()
